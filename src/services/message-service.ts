b8dcf373537d65bbacc82a1d6b6c6dc94d565021
Kenji High <khigh@figure.com>
2021-05-10 15:03:44 -0700

    0.1.26

--- src/services/message-service.ts
+++ src/services/message-service.ts
@@ -1,13 +1,32 @@
 import type { Wallet } from '@tendermint/sig';
 import type { Bytes } from '@tendermint/types';
 import * as google_protobuf_any_pb from 'google-protobuf/google/protobuf/any_pb';
+import { Timestamp } from 'google-protobuf/google/protobuf/timestamp_pb';
 import { Message } from 'google-protobuf';
 import { ecdsaSign as secp256k1EcdsaSign } from 'secp256k1';
 import { createHash } from 'crypto';
 import { base64ToBytes, bufferToBytes, bytesToBase64 } from '@tendermint/belt';
 
 import { log } from '../utils';
-import { AtsMessage, CoinAsObject, MsgSendDisplay, MsgSetWithdrawAddressDisplay, SupportedDenoms } from '../types';
+import {
+  AtsMessage,
+  CoinAsObject,
+  MsgDepositDisplay,
+  MsgFundCommunityPoolDisplay,
+  MsgSendDisplay,
+  MsgSetWithdrawAddressDisplay,
+  MsgUnjailDisplay,
+  MsgVerifyInvariantDisplay,
+  MsgVoteDisplay,
+  MsgWithdrawDelegatorRewardDisplay,
+  MsgWithdrawValidatorCommissionDisplay,
+  SupportedDenoms,
+  MsgEditValidatorDisplay,
+  MsgDelegateDisplay,
+  MsgBeginRedelegateDisplay,
+  MsgUndelegateDisplay,
+  MsgCreateVestingAccountDisplay,
+} from '../types';
 
 import { AuthInfo, Fee, ModeInfo, SignDoc, SignerInfo, Tx, TxBody, TxRaw } from '../proto/cosmos/tx/v1beta1/tx_pb';
 import { BroadcastMode, BroadcastTxRequest, SimulateRequest } from '../proto/cosmos/tx/v1beta1/service_pb';
@@ -35,9 +54,14 @@ import {
   MsgUndelegate,
 } from '../proto/cosmos/staking/v1beta1/tx_pb';
 import { MsgCreateVestingAccount } from '../proto/cosmos/vesting/v1beta1/tx_pb';
+import { CommissionRates, Description } from '../proto/cosmos/staking/v1beta1/staking_pb';
+import { Evidence, Validator } from '../proto/tendermint/abci/types_pb';
+import { Proposal } from '../proto/cosmos/gov/v1beta1/gov_pb';
 
 type SupportedMessageTypeNames =
+  | 'tendermint.abci.Evidence'
   | 'cosmos.bank.v1beta1.MsgSend'
+  | 'cosmos.gov.v1beta1.Proposal'
   | 'cosmwasm.wasm.v1beta1.MsgExecuteContract'
   | 'cosmos.crisis.v1beta1.MsgVerifyInvariant'
   | 'cosmos.distribution.v1beta1.MsgSetWithdrawAddress'
@@ -54,19 +78,60 @@ type SupportedMessageTypeNames =
   | 'cosmos.staking.v1beta1.MsgDelegate'
   | 'cosmos.staking.v1beta1.MsgBeginRedelegate'
   | 'cosmos.staking.v1beta1.MsgUndelegate'
-  | 'cosmos.vesting.v1beta1.MsgCreateVestingAccount';
+  | 'cosmos.vesting.v1beta1.MsgCreateVestingAccount'
+  | 'cosmos.crypto.secp256k1.PubKey';
 
-export type ReadableMessageNames = 'MsgSend' | 'MsgExecuteContract' | 'MsgSetWithdrawAddress';
+export type ReadableMessageNames =
+  | 'PubKey'
+  | 'Evidence'
+  | 'Proposal'
+  | 'MsgSend'
+  | 'MsgExecuteContract'
+  | 'MsgVerifyInvariant'
+  | 'MsgSetWithdrawAddress'
+  | 'MsgWithdrawDelegatorReward'
+  | 'MsgWithdrawValidatorCommission'
+  | 'MsgFundCommunityPool'
+  | 'MsgSubmitEvidence'
+  | 'MsgVote'
+  | 'MsgDeposit'
+  | 'MsgUnjail'
+  | 'MsgCreateValidator'
+  | 'MsgEditValidator'
+  | 'MsgDelegate'
+  | 'MsgBeginRedelegate'
+  | 'MsgUndelegate'
+  | 'MsgCreateVestingAccount';
 
 export type FallbackGenericMessageName = 'MsgGeneric' | 'MsgExecuteContractGeneric';
 
 const TYPE_NAMES_READABLE_MAP: { [key in ReadableMessageNames]: SupportedMessageTypeNames } = {
+  Evidence: 'tendermint.abci.Evidence',
+  PubKey: 'cosmos.crypto.secp256k1.PubKey',
+  Proposal: 'cosmos.gov.v1beta1.Proposal',
   MsgSend: 'cosmos.bank.v1beta1.MsgSend',
   MsgExecuteContract: 'cosmwasm.wasm.v1beta1.MsgExecuteContract',
+  MsgVerifyInvariant: 'cosmos.crisis.v1beta1.MsgVerifyInvariant',
   MsgSetWithdrawAddress: 'cosmos.distribution.v1beta1.MsgSetWithdrawAddress',
+  MsgWithdrawDelegatorReward: 'cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward',
+  MsgWithdrawValidatorCommission: 'cosmos.distribution.v1beta1.MsgWithdrawValidatorCommission',
+  MsgFundCommunityPool: 'cosmos.distribution.v1beta1.MsgFundCommunityPool',
+  MsgSubmitEvidence: 'cosmos.evidence.v1beta1.MsgSubmitEvidence',
+  MsgVote: 'cosmos.gov.v1beta1.MsgVote',
+  MsgDeposit: 'cosmos.gov.v1beta1.MsgDeposit',
+  MsgUnjail: 'cosmos.slashing.v1beta1.MsgUnjail',
+  MsgCreateValidator: 'cosmos.staking.v1beta1.MsgCreateValidator',
+  MsgEditValidator: 'cosmos.staking.v1beta1.MsgEditValidator',
+  MsgDelegate: 'cosmos.staking.v1beta1.MsgDelegate',
+  MsgBeginRedelegate: 'cosmos.staking.v1beta1.MsgBeginRedelegate',
+  MsgUndelegate: 'cosmos.staking.v1beta1.MsgUndelegate',
+  MsgCreateVestingAccount: 'cosmos.vesting.v1beta1.MsgCreateVestingAccount',
 };
 
 const MESSAGE_PROTOS: { [key in SupportedMessageTypeNames]: typeof Message } = {
+  'tendermint.abci.Evidence': Evidence,
+  'cosmos.crypto.secp256k1.PubKey': PubKey,
+  'cosmos.gov.v1beta1.Proposal': Proposal,
   'cosmos.bank.v1beta1.MsgSend': MsgSend,
   'cosmwasm.wasm.v1beta1.MsgExecuteContract': MsgExecuteContract,
   'cosmos.crisis.v1beta1.MsgVerifyInvariant': MsgVerifyInvariant,
@@ -101,6 +166,18 @@ export type MsgExecuteContractDisplay = {
   fundsList: CoinAsObject[];
 };
 
+export type MsgSubmitEvidenceDisplay = Omit<MsgSubmitEvidence.AsObject, 'evidence'> & {
+  evidence: Evidence.AsObject;
+};
+
+// export type MsgSubmitProposalDisplay = Omit<MsgSubmitProposal.AsObject, 'content'> & {
+//   content: Proposal.AsObject;
+// };
+
+export type MsgCreateValidatorDisplay = Omit<MsgCreateValidator.AsObject, 'pubkey'> & {
+  pubkey: PubKey.AsObject;
+};
+
 export type GenericDisplay = { [key: string]: any };
 
 export class MessageService {
@@ -111,7 +188,27 @@ export class MessageService {
     return google_protobuf_any_pb.Any.deserializeBinary(base64ToBytes(msgAnyB64));
   }
 
-  buildMessage(type: ReadableMessageNames, params: MsgSendDisplay | MsgExecuteContractParams | MsgSetWithdrawAddressDisplay): Message {
+  buildMessage(
+    type: ReadableMessageNames,
+    params:
+      | MsgSendDisplay
+      | MsgExecuteContractParams
+      | MsgVerifyInvariantDisplay
+      | MsgSetWithdrawAddressDisplay
+      | MsgWithdrawDelegatorRewardDisplay
+      | MsgWithdrawValidatorCommissionDisplay
+      | MsgFundCommunityPoolDisplay
+      | MsgSubmitEvidenceDisplay
+      | MsgVoteDisplay
+      | MsgDepositDisplay
+      | MsgUnjailDisplay
+      | MsgCreateValidatorDisplay
+      | MsgEditValidatorDisplay
+      | MsgDelegateDisplay
+      | MsgBeginRedelegateDisplay
+      | MsgUndelegateDisplay
+      | MsgCreateVestingAccountDisplay
+  ): Message {
     switch (type) {
       case 'MsgSend': {
         const { fromAddress, toAddress, amountList } = params as MsgSendDisplay;
@@ -135,9 +232,165 @@ export class MessageService {
           });
         return msgExecuteContract;
       }
+      case 'MsgVerifyInvariant': {
+        const { sender, invariantRoute, invariantModuleName } = params as MsgVerifyInvariantDisplay;
+        return new MsgVerifyInvariant()
+          .setSender(sender)
+          .setInvariantRoute(invariantRoute)
+          .setInvariantModuleName(invariantModuleName);
+      }
       case 'MsgSetWithdrawAddress': {
         const { delegatorAddress, withdrawAddress } = params as MsgSetWithdrawAddressDisplay;
         return new MsgSetWithdrawAddress().setWithdrawAddress(withdrawAddress).setDelegatorAddress(delegatorAddress);
+      }
+      case 'MsgWithdrawDelegatorReward': {
+        const { delegatorAddress, validatorAddress } = params as MsgWithdrawDelegatorRewardDisplay;
+        return new MsgWithdrawDelegatorReward().setValidatorAddress(validatorAddress).setDelegatorAddress(delegatorAddress);
+      }
+      case 'MsgWithdrawValidatorCommission': {
+        const { validatorAddress } = params as MsgWithdrawValidatorCommissionDisplay;
+        return new MsgWithdrawValidatorCommission().setValidatorAddress(validatorAddress);
+      }
+      case 'MsgFundCommunityPool': {
+        const { amountList, depositor } = params as MsgFundCommunityPoolDisplay;
+        const msgFundCommunityPool = new MsgFundCommunityPool().setDepositor(depositor);
+        amountList.forEach(({ denom, amount }) => {
+          msgFundCommunityPool.addAmount(new Coin().setAmount(`${amount}`).setDenom(denom));
+        });
+        return msgFundCommunityPool;
+      }
+      case 'MsgSubmitEvidence': {
+        const { submitter, evidence } = params as MsgSubmitEvidenceDisplay;
+        const msgSubmitEvidence = new MsgSubmitEvidence().setSubmitter(submitter);
+        if (evidence) {
+          const { type, time, totalVotingPower, height, validator } = evidence;
+          const theEvidence = new Evidence().setHeight(height).setTotalVotingPower(totalVotingPower).setType(type);
+          if (validator) {
+            theEvidence.setValidator(new Validator().setAddress(validator.address).setPower(validator.power));
+          }
+          if (time) {
+            theEvidence.setTime(new Timestamp().setSeconds(time.seconds).setNanos(time.nanos));
+          }
+          const evidenceAny = new google_protobuf_any_pb.Any();
+          evidenceAny.pack(theEvidence.serializeBinary(), TYPE_NAMES_READABLE_MAP.Evidence, '/');
+          msgSubmitEvidence.setEvidence(evidenceAny);
+        }
+        return msgSubmitEvidence;
+      }
+      case 'MsgVote': {
+        const { proposalId, voter, option } = params as MsgVoteDisplay;
+        return new MsgVote().setProposalId(proposalId).setVoter(voter).setOption(option);
+      }
+      case 'MsgDeposit': {
+        const { proposalId, depositor, amountList } = params as MsgDepositDisplay;
+        const msgDeposit = new MsgDeposit().setDepositor(depositor).setProposalId(proposalId);
+        amountList.forEach(({ denom, amount }) => {
+          msgDeposit.addAmount(new Coin().setAmount(`${amount}`).setDenom(denom));
+        });
+        return msgDeposit;
+      }
+      case 'MsgUnjail': {
+        const { validatorAddr } = params as MsgUnjailDisplay;
+        return new MsgUnjail().setValidatorAddr(validatorAddr);
+      }
+      case 'MsgCreateValidator': {
+        const {
+          pubkey,
+          value,
+          validatorAddress,
+          delegatorAddress,
+          commission,
+          minSelfDelegation,
+          description,
+        } = params as MsgCreateValidatorDisplay;
+        const msgCreateValidator = new MsgCreateValidator()
+          .setValidatorAddress(validatorAddress)
+          .setDelegatorAddress(delegatorAddress)
+          .setMinSelfDelegation(minSelfDelegation);
+        if (description) {
+          const { moniker, identity, website, securityContact, details } = description;
+          msgCreateValidator.setDescription(
+            new Description()
+              .setMoniker(moniker)
+              .setIdentity(identity)
+              .setWebsite(website)
+              .setSecurityContact(securityContact)
+              .setDetails(details)
+          );
+        }
+        if (value) {
+          msgCreateValidator.setValue(new Coin().setAmount(value.amount).setDenom(value.denom));
+        }
+        if (commission) {
+          msgCreateValidator.setCommission(
+            new CommissionRates().setRate(commission.rate).setMaxChangeRate(commission.maxChangeRate).setMaxRate(commission.maxRate)
+          );
+        }
+        if (pubkey) {
+          const key = new PubKey().setKey(pubkey.key);
+          const pubKeyAny = new google_protobuf_any_pb.Any();
+          pubKeyAny.pack(key.serializeBinary(), TYPE_NAMES_READABLE_MAP.PubKey, '/');
+          msgCreateValidator.setPubkey(pubKeyAny);
+        }
+        return msgCreateValidator;
+      }
+      case 'MsgEditValidator': {
+        const { description, validatorAddress, commissionRate, minSelfDelegation } = params as MsgEditValidatorDisplay;
+        const msgEditValidator = new MsgEditValidator()
+          .setValidatorAddress(validatorAddress)
+          .setCommissionRate(commissionRate)
+          .setMinSelfDelegation(minSelfDelegation);
+        if (description) {
+          const { moniker, identity, website, securityContact, details } = description;
+          msgEditValidator.setDescription(
+            new Description()
+              .setMoniker(moniker)
+              .setIdentity(identity)
+              .setWebsite(website)
+              .setSecurityContact(securityContact)
+              .setDetails(details)
+          );
+        }
+        return msgEditValidator;
+      }
+      case 'MsgDelegate': {
+        const { delegatorAddress, validatorAddress, amount } = params as MsgDelegateDisplay;
+        const msgDelegate = new MsgDelegate().setDelegatorAddress(delegatorAddress).setValidatorAddress(validatorAddress);
+        if (amount) {
+          msgDelegate.setAmount(new Coin().setAmount(`${amount.amount}`).setDenom(amount.denom));
+        }
+        return msgDelegate;
+      }
+      case 'MsgBeginRedelegate': {
+        const { delegatorAddress, validatorDstAddress, validatorSrcAddress, amount } = params as MsgBeginRedelegateDisplay;
+        const msgBeginRedelegate = new MsgBeginRedelegate()
+          .setDelegatorAddress(delegatorAddress)
+          .setValidatorDstAddress(validatorDstAddress)
+          .setValidatorSrcAddress(validatorSrcAddress);
+        if (amount) {
+          msgBeginRedelegate.setAmount(new Coin().setAmount(`${amount.amount}`).setDenom(amount.denom));
+        }
+        return msgBeginRedelegate;
+      }
+      case 'MsgUndelegate': {
+        const { delegatorAddress, validatorAddress, amount } = params as MsgUndelegateDisplay;
+        const msgUndelegate = new MsgUndelegate().setDelegatorAddress(delegatorAddress).setValidatorAddress(validatorAddress);
+        if (amount) {
+          msgUndelegate.setAmount(new Coin().setAmount(`${amount.amount}`).setDenom(amount.denom));
+        }
+        return msgUndelegate;
+      }
+      case 'MsgCreateVestingAccount': {
+        const { fromAddress, toAddress, endTime, delayed, amountList } = params as MsgCreateVestingAccountDisplay;
+        const msgCreateVestingAccount = new MsgCreateVestingAccount()
+          .setFromAddress(fromAddress)
+          .setToAddress(toAddress)
+          .setEndTime(endTime)
+          .setDelayed(delayed);
+        amountList.forEach(({ denom, amount }) => {
+          msgCreateVestingAccount.addAmount(new Coin().setAmount(`${amount}`).setDenom(denom));
+        });
+        return msgCreateVestingAccount;
       }
       default:
         throw new Error(`Message type: ${type} is not supported.`);
@@ -250,7 +503,7 @@ export class MessageService {
     pubKey.setKey(pubKeyBytes);
     const pubKeyAny = new google_protobuf_any_pb.Any();
     log(`PubKey displayName: ${(PubKey as any).displayName}`);
-    pubKeyAny.pack(pubKey.serializeBinary(), 'cosmos.crypto.secp256k1.PubKey', '/');
+    pubKeyAny.pack(pubKey.serializeBinary(), TYPE_NAMES_READABLE_MAP.PubKey, '/');
     signerInfo.setPublicKey(pubKeyAny);
     signerInfo.setModeInfo(modeInfo);
     signerInfo.setSequence(baseAccount.getSequence());
--- src/types/messages.ts
+++ src/types/messages.ts
@@ -6,6 +6,11 @@ import type {
   MsgWithdrawValidatorCommission,
   MsgFundCommunityPool,
 } from '../proto/cosmos/distribution/v1beta1/tx_pb';
+import { MsgVote, MsgDeposit } from '../proto/cosmos/gov/v1beta1/tx_pb';
+import { MsgUnjail } from '../proto/cosmos/slashing/v1beta1/tx_pb';
+import { MsgEditValidator, MsgDelegate, MsgBeginRedelegate, MsgUndelegate } from '../proto/cosmos/staking/v1beta1/tx_pb';
+import { MsgCreateVestingAccount } from '../proto/cosmos/vesting/v1beta1/tx_pb';
+import { Proposal } from '../proto/cosmos/gov/v1beta1/gov_pb';
 
 export type MsgSendDisplay = MsgSend.AsObject;
 export type MsgVerifyInvariantDisplay = MsgVerifyInvariant.AsObject;
@@ -13,3 +18,12 @@ export type MsgSetWithdrawAddressDisplay = MsgSetWithdrawAddress.AsObject;
 export type MsgWithdrawDelegatorRewardDisplay = MsgWithdrawDelegatorReward.AsObject;
 export type MsgWithdrawValidatorCommissionDisplay = MsgWithdrawValidatorCommission.AsObject;
 export type MsgFundCommunityPoolDisplay = MsgFundCommunityPool.AsObject;
+export type ProposalDisplay = Proposal.AsObject;
+export type MsgVoteDisplay = MsgVote.AsObject;
+export type MsgDepositDisplay = MsgDeposit.AsObject;
+export type MsgUnjailDisplay = MsgUnjail.AsObject;
+export type MsgEditValidatorDisplay = MsgEditValidator.AsObject;
+export type MsgDelegateDisplay = MsgDelegate.AsObject;
+export type MsgBeginRedelegateDisplay = MsgBeginRedelegate.AsObject;
+export type MsgUndelegateDisplay = MsgUndelegate.AsObject;
+export type MsgCreateVestingAccountDisplay = MsgCreateVestingAccount.AsObject;
